783de92d8d3b81ca9b5c51ab2e8bd12c42ac406f
Kouhei Sutou <kou@clear-code.com>
2017-05-16 17:42:28 +0900

    Show the original values

--- sample/write-tensor.py
+++ sample/write-tensor.py
@@ -3,6 +3,8 @@
 import pyarrow as pa
 import numpy as np
 
-tensor = pa.Tensor.from_numpy(np.random.randn(10, 6))
+ndarray = np.random.randn(10, 6)
+print(ndarray)
+tensor = pa.Tensor.from_numpy(ndarray)
 with pa.OSFile("/tmp/tensor.arrow", "wb") as sink:
     pa.write_tensor(tensor, sink)

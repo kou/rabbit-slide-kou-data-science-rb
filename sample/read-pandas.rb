47c591c1dcf6c12e5c076bd392d23ee3456f691c
Kouhei Sutou <kou@clear-code.com>
2017-05-18 13:52:56 +0900

    Fix class

--- sample/read-pandas.rb
+++ sample/read-pandas.rb
@@ -3,7 +3,7 @@
 require "arrow"
 
 Arrow::MemoryMappedInputStream.open("/tmp/pandas.arrow") do |input|
-  reader = Arrow::FileReader.new(input)
+  reader = Arrow::RecordBatchFileReader.new(input)
   reader.each do |record_batch|
     puts("=" * 40)
     puts(record_batch)
